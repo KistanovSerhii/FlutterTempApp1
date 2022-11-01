fb961381d08e9147cc7c2181d1f7a14be3741c6c
Serhii <Kistanov.serhii@yandex.ru>
2022-11-01 13:02:11 +0300

    Delete lib directory

--- lib/domain/contracts/c_chessboard.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:smart_chess_board/domain/models/m_turn.dart';
-import 'package:smart_chess_board/domain/state/devicemodules/bluetooth.dart';
-
-// Интерфейс-Контракт
-abstract class ChessBoardContracr {
-  // Методы который обязаны быть реализованы!
-  Future<TurnModel> getChessBoard({@required BlueDevice device});
-}
